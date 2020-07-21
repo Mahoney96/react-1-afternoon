d4ad112131a71d74586f9aea52e42a2bba1e3eb2
Mahoney96 <cmahoney966@gmail.com>
2020-07-21 14:35:35 -0700

    initial React day1 afternoon project save. Debugged Node issues occurring

---
+++ src/components/Topics/Palindrome.js
@@ -0,0 +1,9 @@
+import React, {Component } from 'react';
+
+export default class Panlindrome extends Component {
+    render(){
+        return(
+            <p> Panlindrome Component</p>
+        )
+    }
+}
