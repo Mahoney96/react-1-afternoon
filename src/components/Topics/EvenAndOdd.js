c560b1399458133ab92257044098ea1dcda6e344
Mahoney96 <cmahoney966@gmail.com>
2020-07-22 02:36:53 -0700

    step 3 progress

--- src/components/Topics/EvenAndOdd.js
+++ src/components/Topics/EvenAndOdd.js
@@ -1,9 +1,23 @@
 import React, {Component } from 'react';
 
 export default class EvenAndOdd extends Component {
+    constructor(){
+        super()
+        this.state ={
+            evenArray: [],
+            oddArray: [],
+            userInput:
+        }
+    }
     render(){
         return (
-            <p> EvenAndOdd Component</p>
+            <div className="puzzleBox evenAndOddPB">
+                <h4> Evens and Odds</h4>
+                <input className="inputline" onChange={ (e) => this.handleChange(e.target.value) }/>
+                <button className="confrimationButton"> Split </button>
+                <span className="resultsBox"> Evens: { JSON.stringify(this.state.evenArray) }</span>
+                <span className="resultsBox"> Odds: {JSON.stringify(this.state.oddArray) }</span>
+            </div>
         )
     }
 }
