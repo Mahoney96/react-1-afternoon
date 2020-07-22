8d9047efe3e06016d3cd60ae53eb375a85e36b9f
Mahoney96 <cmahoney966@gmail.com>
2020-07-21 21:59:02 -0700

    additonal progress

--- src/components/Topics/EvenAndOdd.js
+++ src/components/Topics/EvenAndOdd.js
@@ -2,7 +2,15 @@ import React, {Component } from 'react';
 export default class EvenAndOdd extends Component {
     render() {
         return (
-            <p> EvenAndOdd Component</p>
+            <div className="puzzleBox evenAndOddPB">
+                <h4> Evens and Odds </h4>
+                <input className ="inputLine"/>
+                <button className="confirmationButton"> Split </button>
+                <span className="resultsBox"></span>
+                <span className="resultsBox"></span>
+
+
+            </div>
         )
     }
 }
